fef0e7d7f044d24995132b0df7a075473e581e91
Parth Panchal <parthpanchal1352003@gmail.com>
2024-11-17 19:41:03 +0530

    Added new features to the To-Do list app

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+export default {
+  output: "export", // Enables static export
+  basePath: "/ToDo-List-Using-React-Next.js", // Replace <repository-name> with your GitHub repo name
+};
